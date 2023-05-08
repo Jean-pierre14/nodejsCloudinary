e370b23a99ff524a8996a6128f0517d766d390a3
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-05-08 10:54:38 +0300

    CRUD operators

--- controllers/indexcontrollers.js
+++ controllers/indexcontrollers.js
@@ -1,5 +1,19 @@
-const postData = (request, response) => {
+export const postData = (request, response) => {
   const { username } = request.body;
+
   const file = request.files.image;
+
   response.json(request.body);
+};
+
+export const getData = async (request, response) => {
+  await response.json({ message: "API data" });
+};
+
+export const updateData = async (request, response) => {
+  await response.json({ message: "API update data" });
+};
+
+export const deleteData = async (request, response) => {
+  await response.json({ message: "API delete data" });
 };
